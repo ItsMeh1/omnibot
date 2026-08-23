fed8f4e37d196f95c78de090b17b8472b9205eb2
ItsMeh1 <omarabuelfoutouh@gmail.com>
2026-08-23 12:11:13 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Set this after deploying the Cloudflare Worker.
 // Example: https://omnibot.your-account.workers.dev
-window.OMNIBOT_API = 'YOUR_CLOUDFLARE_WORKER_URL';
+window.OMNIBOT_API = 'omnibot.omarabuelfoutouh.workers.dev
+';
